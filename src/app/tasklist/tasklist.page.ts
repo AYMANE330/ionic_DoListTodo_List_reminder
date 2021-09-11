e5cde761dea9b8290013ff2b4e39a3bc2e535ab2
AYMANE330 <aymanefilali330@gmail.com>
2021-09-11 15:52:01 +0100

    update

--- src/app/tasklist/tasklist.page.ts
+++ src/app/tasklist/tasklist.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { AlertController, ModalController, IonTabs } from '@ionic/angular';
+import { AlertController, ModalController, IonTabs, IonItemSliding } from '@ionic/angular';
 import { TodoService } from '../todo.service';
 import { UpdateTaskPage } from '../update-task/update-task.page';
 
@@ -13,7 +13,7 @@ export class TasklistPage {
 
   today: number = Date.now();
 
-  constructor(public modalCtlr: ModalController, public todoService: TodoService) {
+  constructor(public modalCtlr: ModalController, public todoService: TodoService, public alertController: AlertController) {
     this.getAllTask()
   }
 
@@ -32,8 +32,35 @@ export class TasklistPage {
     console.log(this.todoService.getAllTasks());
   }
 
-  delete(key) {
+  confirmDelete(key) {
     this.todoService.deleteTask(key);
+  }
+
+  async delete(key, item: IonItemSliding) {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Confirm!',
+      message: 'Message <strong>text</strong>!!!',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+              item.close();
+          }
+        }, {
+          text: 'Okay',
+          handler: () => {
+            this.confirmDelete(key);
+            console.log('Confirm Okay');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
   }
 
   async update(selectedTask) {
